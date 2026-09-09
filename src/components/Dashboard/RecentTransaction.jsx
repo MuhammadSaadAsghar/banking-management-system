892e31f02c7e864e990a7d4d0019a596423b940b
Muhammad Saad Asghar <muhammadsaadasghar408@gmail.com>
2026-09-09 18:41:10 +0500

    responsive dashboard

--- src/components/Dashboard/RecentTransaction.jsx
+++ src/components/Dashboard/RecentTransaction.jsx
@@ -85,19 +85,19 @@ const RecentTransaction = () => {
           <table className="w-full mt-4">
             <thead className="border-b border-t bg-gray-200  border-gray-200">
               <tr >
-                <th className="text-left px-5 py-3 text-xs font-medium text-gray-500">
+                <th className="text-left lg:px-5 px-2 py-3 text-xs font-medium text-gray-500">
                   Date
                 </th>
-                <th className="text-left py-5 px-3 text-xs font-medium text-gray-500">
+                <th className="text-left py-5 lg:px-3 px-2 text-xs font-medium text-gray-500">
                   Type
                 </th>
-                <th className="text-left py-5 px-3 text-xs font-medium text-gray-500">
+                <th className="text-left py-5 lg:px-3 px-2 text-xs font-medium text-gray-500">
                   Account
                 </th>
-                <th className="text-left py-5 px-3 text-xs font-medium text-gray-500">
+                <th className="text-left py-5 lg:px-3 px-2 text-xs font-medium text-gray-500">
                   Amount
                 </th>
-                <th className="text-left py-5 px-3 text-xs font-medium text-gray-500">
+                <th className="text-left py-5 lg:px-3 px-2 text-xs font-medium text-gray-500">
                   Status
                 </th>
               </tr>
@@ -106,17 +106,17 @@ const RecentTransaction = () => {
         {recentTransactions.map((ele) => {
             const Icon = ele.icon;
           return  <tr key={ele.id} className="border-b   border-gray-100">
-                <td className="px-3 py-2 text-sm text-gray-700">
+                <td className="lg:px-3 px-1 py-2 text-sm text-gray-700">
                  {ele.date}
                 </td>
-                <td className={` py-2 px-2 text-sm text-gray-700 `}> <div className="flex items-center gap-2"> <span className={`${ele.iconColor}`} ><Icon/></span>{ele.type}</div></td>
-                <td className=" py-2 px-2 text-sm text-gray-700 ">
+                <td className={` py-2 px-1 lg:px-2 text-sm text-gray-700 `}> <div className="flex items-center gap-2"> <span className={`${ele.iconColor}`} ><Icon/></span>{ele.type}</div></td>
+                <td className=" py-2 px-1 lg:px-2 text-sm text-gray-700 ">
                  {ele.account}
                 </td>
-                <td className={`py-2 px-2 text-sm text-gray-700 ${ele.amountColor}`}>
+                <td className={`py-2 px-1 lg:px-2 text-sm text-gray-700 ${ele.amountColor}`}>
                   {ele.amount}
                 </td>
-                                  <td className="px-3 py-2">
+                                  <td className="lg:px-3 px-1 py-2">
                     <span className="rounded-full bg-green-100 px-3 py-1 text-xs font-medium text-green-600">
                       {ele.status}
                     </span>
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -16,10 +16,10 @@ const Dashboard = () => {
     </section>
   <Dashboardcard/>
   <div className='grid grid-cols-1 lg:grid-cols-5 gap-3 items-stretch'>
-    <div className='lg:col-span-3'>
+    <div className='lg:col-span-3 grid-cols-1'>
   <RecentTransaction/>
   </div>
-  <div className='lg:col-span-2 h-full'>
+  <div className='lg:col-span-2 grid-cols-1 h-full'>
   <Quickactions/>
   </div>
   </div>
